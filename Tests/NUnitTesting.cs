177048c6e2b93fc20d6d981b1c8822fb05eff110
mateusz <mateuszmajer1992@gmail.com>
2018-11-21 22:23:28 +0100

    Finish prepare UnitTestsEnviroment

---
+++ Tests/NUnitTesting.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Testing
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
